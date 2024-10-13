ba7812d76a5d4202f3faffef1e7a4b95e0268556
Jorzuiz <jordazurdo@hotmail.com>
2024-10-13 11:53:27 +0200

    AA P1 Progress
    THE NEXT DAY
    
                   There is a line of fairy tale creatures. The head of the guard
                   sits at a table paying people for bringing the fairy tale creatures
                   to him. There are cages all around. Some of the people in line
                   are Peter Pan, who is carrying Tinkerbell in a cage, Gipetto
                   who's carrying Pinocchio, and a farmer who is carrying the three
                   little pigs.
    
                                         GUARD
                             All right. This one's full. Take it
                             away! Move it along. Come on! Get up!
    
                                         HEAD GUARD
                             Next!
    
                                         GUARD
                             (taking the witch's broom) Give me that!
                             Your flying days are over. (breaks the
                             broom in half)
    
                                         HEAD GUARD
                             That's 20 pieces of silver for the witch.
                             Next!
    
                                         GUARD
                             Get up! Come on!
    
                                         HEAD GUARD
                             Twenty pieces.
    
                                         LITTLE BEAR
                             (crying) This cage is too small.
    
                                         DONKEY
                             Please, don't turn me in. I'll never
                             be stubborn again. I can change. Please!
                             Give me another chance!
    
                                         OLD WOMAN
                             Oh, shut up. (jerks his rope)
    
                                         DONKEY
                             Oh!
    
                                         HEAD GUARD
                             Next! What have you got?
    
                                         GIPETTO
                             This little wooden puppet.
    
                                         PINOCCHIO
                             I'm not a puppet. I'm a real boy. (his
                             nose grows)
    
                                         HEAD GUARD
                             Five shillings for the possessed toy.
                             Take it away.
    
                                         PINOCCHIO
                             Father, please! Don't let them do this!
                             Help me!

--- AA/Práctias/P1/LinearRegression.py
+++ AA/Práctias/P1/LinearRegression.py
@@ -3,6 +3,7 @@ import copy
 import math
 
 class LinearReg:
+    
     """
     Computes the cost function for linear regression.
 
@@ -11,10 +12,12 @@ class LinearReg:
         y (ndarray): Shape (m,) the real values of the prediction
         w, b (scalar): Parameters of the model
     """
-    def __init__(self, x, y,w,b):
-        #(scalar): Parameters of the model
-        return #delete this return
-
+    #Self es la variable interna de la clase
+    def __init__(self, x, y, w, b):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.b = b
 
     """
     Computes the linear regression function.
@@ -26,6 +29,7 @@ class LinearReg:
         the linear regression value
     """
 
+    # Esto ya viene hecho?
     def f_w_b(self, x):
         return self.w * x + self.b
 
@@ -37,25 +41,39 @@ class LinearReg:
                to fit the data points in x and y
     """
     def compute_cost(self):
-        total_cost = 0
-
-
-        return total_cost
+        
+        m = len(y) # Tamaño de la entrada
+        error = self.w * self.x + self.b # Valor esperado
     
+        # Esta es la formula de coste de la funcion
+        # Mean Squared Error formula
+        squared_error = (error-self.y) **2 
+    
+        total_cost = (1/(2*m)) * np.sum(squared_error)
+    
+        # No sé que representa o significa self
+        return total_cost    
 
     """
     Computes the gradient for linear regression 
     Args:
+      x (ndarray): Shape (m,) Input to the model (Population of cities) 
+      y (ndarray): Shape (m,) Label (Actual profits for the cities)
+      w, b (scalar): Parameters of the model  
 
     Returns
       dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
       dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
      """
     def compute_gradient(self):
-        dj_dw = 0
-        dj_db = 0
-        return dj_dw, dj_db
+        m = len(self.y)
+        hypothesis = self.w * self.x + self.b
+
+        dj_dw = np.sum((hypothesis-self.y)*self.x)/m
     
+        dj_db = np.sum((hypothesis-self.y))/m
+    
+        return dj_dw, dj_db
     """
     Performs batch gradient descent to learn theta. Updates theta by taking 
     num_iters gradient steps with learning rate alpha
@@ -75,11 +93,17 @@ class LinearReg:
     """
     def gradient_descent(self, alpha, num_iters):
         # An array to store cost J and w's at each iteration — primarily for graphing later
-        J_history = []
+        J_history = np.zeros(num_iters)
         w_history = []
         w_initial = copy.deepcopy(self.w)  # avoid modifying global w within function
         b_initial = copy.deepcopy(self.b)  # avoid modifying global b within function
         
+        for i in range(num_iters):
+            dj_dw, dj_db = self.compute_gradient(self)
+            self.w-=alpha*dj_dw
+            self.b-=alpha*dj_db
+            J_history[i]= self.compute_cost(self)
+
         return self.w, self.b, J_history, w_initial, b_initial
 
 
--- AA/Práctias/P1/utils.py
+++ AA/Práctias/P1/utils.py
@@ -2,9 +2,21 @@ import numpy as np
 import pandas as pd
 
 
-def cleanData(data):
-   #TO-DO clean the dataframe. Return the dataframe cleaned.
-    return data
+def cleanData(gamesData):
+    # numeros -> score(0-100), user score(0.0-10.0), critics users
+
+    #limpia valores
+    gamesData['score'] = gamesData['score'].astype(np.float32)
+    #gamesData['score'].fillna(gamesData['score'].mean(), inplace = True)
+    #gamesData['score']= pd.to_numeric(gamesData['score'], errors='coerce')
+    gamesData['score']=gamesData['score'].div(10)
+
+    # Limpia valores tbd y convierte en float32
+    gamesData['user score'].replace('tbd', np.nan, inplace = True)
+    gamesData['user score'].fillna(np.nan)
+    gamesData['user score'] = gamesData['user score'].astype(np.float32)
+
+    return gamesData
 
 def load_data_csv(path,x_colum,y_colum):
     data = pd.read_csv(path)
